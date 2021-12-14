4f7628feb30804e69cb55b7b2088d89532fef72b
Matthew Wilson <matthew.wilson@instil.co>
2021-12-14 14:59:15 +0000

    Initial ccommit

--- lib/appsync-demo-stack.ts
+++ lib/appsync-demo-stack.ts
@@ -1,15 +1,49 @@
-import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as cdk from "@aws-cdk/core";
+import * as path from "path";
+import {AuthorizationType, GraphqlApi, MappingTemplate, PrimaryKey, Schema, Values} from "@aws-cdk/aws-appsync";
+import {AttributeType, Table} from "@aws-cdk/aws-dynamodb";
 
 export class AppsyncDemoStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const api = new GraphqlApi(this, "Api", {
+      name: "demo",
+      schema: Schema.fromAsset(path.join(__dirname, "schema.graphql")),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: AuthorizationType.API_KEY,
+        },
+      },
+      xrayEnabled: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppsyncDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const demoTable = new Table(this, "DemoTable", {
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING,
+      },
+    });
+
+    const demoDS = api.addDynamoDbDataSource("demoDataSource", demoTable);
+
+    // Resolver for the Query "getDemos" that scans the DynamoDb table and returns the entire list.
+    demoDS.createResolver({
+      typeName: "Query",
+      fieldName: "getDemos",
+      requestMappingTemplate: MappingTemplate.dynamoDbScanTable(),
+      responseMappingTemplate: MappingTemplate.dynamoDbResultList(),
+    });
+
+    // Resolver for the Mutation "addDemo" that puts the item into the DynamoDb table.
+    demoDS.createResolver({
+      typeName: "Mutation",
+      fieldName: "addDemo",
+      requestMappingTemplate: MappingTemplate.dynamoDbPutItem(
+          PrimaryKey.partition("id").auto(),
+          Values.projecting("input"),
+      ),
+      responseMappingTemplate: MappingTemplate.dynamoDbResultItem(),
+    });
   }
 }
